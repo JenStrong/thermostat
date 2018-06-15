c44338a28173f5a14657834afd1b9b6e5a2a4e37
Jen Strong <jenniferastrong@gmail.com>
2018-06-15 11:49:00 +0100

    added functionality to choose city to see temp

--- interface.js
+++ interface.js
@@ -2,9 +2,20 @@ $(document).ready(function(){
   var thermostat = new Thermostat();
   updateTemperature();
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3c4637f8334e84b45a1b5e450fae1ab0&units=imperial', function(data) {
-  $('#current-temperature').text(data.main.temp);
+  $('#select-city').submit(function(event){
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
   })
+
+  function displayWeather(city){
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=3c4637f8334e84b45a1b5e450fae1ab0';
+    var units = '&units=imperial';
+    $.get(url + token +units, function(data){
+      $('#current-temperature').text(data.main.temp);
+    })
+  };
 
   $('#temperature-up').on('click', function(){
     thermostat.up();
