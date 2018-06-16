feb0dd777da489b77080a37c08a7ad2e0e0d4129
Jen Strong <jenniferastrong@gmail.com>
2018-06-16 17:30:45 +0100

    added html and css

--- src/thermostat.js
+++ src/thermostat.js
@@ -59,7 +59,7 @@ Thermostat.prototype.energyUsage = function () {
   if (this.temperature < 18){
     return 'low-usage';
   }
-  else if (this.temperature >18 && this.temperature <25){
+  else if (this.temperature >=18 && this.temperature <25){
     return 'medium-usage';
   }
   else{
