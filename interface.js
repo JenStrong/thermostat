384c1a79d3ed927e1b2262ddad5d268a0d70780e
Jen Strong <jenniferastrong@gmail.com>
2018-07-27 17:41:31 +0100

    Remove weather search functionality

--- interface.js
+++ interface.js
@@ -2,26 +2,6 @@ $(document).ready(function(){
   var thermostat = new Thermostat();
   updateTemperature();
 
-  $('#select-city').submit(function(event){
-    event.preventDefault();
-    var city = $('#current-city').val();
-    displayWeather(city);
-  })
-
-  $("#get-city").click(function() {
-    var inputCity = $('#current-city').val();
-    $('#display-city').text(inputCity);
-  });
-
-  function displayWeather(city){
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
-    var token = '&appid=3c4637f8334e84b45a1b5e450fae1ab0';
-    var units = '&units=imperial';
-    $.get(url + token + units, function(data){
-      $('#current-temperature').text(data.main.temp);
-    })
-  };
-
   $('#temperature-up').click(function(){
     thermostat.up();
     updateTemperature();
